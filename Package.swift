c0cb3cc8ecfe8a3bddd3b0d11b820c758bb432de
Tomohiro Kumagai <tomo@ez-net.jp>
2024-06-17 01:22:42 +0900

    Rename project and executable file.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SharePrivateCalendar",
+    name: "PrivacyProtectedCalendar",
     platforms: [
         .macOS(.v13),
         .custom("linux", versionString: "5.4.0")
@@ -16,7 +16,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "SharePrivateCalendar",
+            name: "privacy-protected-calendar",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser")
             ]
